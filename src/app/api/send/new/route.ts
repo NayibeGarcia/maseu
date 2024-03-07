227275c9d9a280e046ae224f62dc10e93511dfca
Karen Garcia <nayibe9520@gmail.com>
2024-03-06 23:49:13 -0500

    modificacion plantilla de correo

--- src/app/api/send/new/route.ts
+++ src/app/api/send/new/route.ts
@@ -4,40 +4,66 @@ import { transporter, mailOptions } from '@/nodemailer'
 // Handles POST requests to /api
 
 const htmlContent = (data: any) => `
-  <html>
-    <head>
-      <style>
+<html>
+
+<head>
+    <style>
         body {
-          font-family: Arial, sans-serif;
+            font-family: Arial, sans-serif;
         }
+
         .header {
-          background-color: #3498db;
-          color: white;
-          padding: 10px;
-          text-align: center;
+            padding: 10px;
+            text-align: center;
         }
+        .header img{
+          width: 70%;
+          margin-left: 15%;
+          margin-right: 15%;
+      }
+
         .content {
-          padding: 20px;
+            padding: 20px;
+            margin-left: 15%;
+            margin-right: 15%;
         }
-        .saludo{
-          font-weight: 600;
+
+        .saludo {
+            font-weight: 600;
         }
-      </style>
-    </head>
-    <body>
-      <div class="header">
-        <h1>Se Creo la solicitud</h1>
-      </div>
-      <div> 
-        <p class="saludo">
-        Hola ${data.name}
-        </p>  
-        <p>
-          te confirmamos que tu cotizacion a sido enviado pronto nos estaremos cominicando contigo 
-        </p>
-      </div>
-    </body>
-  </html>
+        .informacion{
+            background-color: #cfcfcf;
+            border-radius: 15px;
+            margin: 15px 0;
+            padding: 15px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="header">
+        <img src="https://firebasestorage.googleapis.com/v0/b/mas-eu-6aa58.appspot.com/o/sitio%2FEncabezado.png?alt=media&token=036121e9-4fc8-44e6-bc0a-033f216ab20a"
+            alt="Encabezado" />
+    </div>
+    <div class="content">
+        <p class="saludo">Estimado cliente ${data.name},</p>
+        <p>Le informamos que hemos creado su solicitud de cotización. En nuestra empresa, nos esforzamos por brindar un
+            servicio confiable y de alta calidad. Nuestro equipo está comprometido con la satisfacción del cliente y la
+            transparencia en cada paso del proceso. Pronto uno de nuestros asesores se estara comunicando con usted.</p>
+        <p>Si tiene alguna pregunta o necesita más detalles, no dude en ponerse en contacto con nosotros. ¡Gracias por
+            confiar en nosotros!</p>
+
+        <div class="informacion">
+           <strong>Mantenimientos Asesorías y Servicios E.U.</strong>
+        <p><strong>Telefax:</strong> 6412880</p>
+        <p><strong>Teléfono:</strong> 6444372</p>
+        <p><strong>Celular:</strong> 3102554944</p> 
+        </div>
+        
+    </div>
+</body>
+
+</html>
 `
 
 export async function POST(request: Request) {
@@ -46,7 +72,7 @@ export async function POST(request: Request) {
   await transporter.sendMail({
     from: mailOptions.from, // sender address
     to: data.email, // list of receivers
-    subject: `Notificacion: Cotizacion M.A.S`, // Subject line
+    subject: `Notificación: Cotizacion M.A.S`, // Subject line
     text: `M.A.S`, // plain text body
     // html: `<div>Hola ${data.name} te confirmamos que tu cotizacion a sido enviado pronto nos estaremos cominicando contigo </div>`, // html body
     html: htmlContent(data),
--- src/app/api/send/update/route.ts
+++ src/app/api/send/update/route.ts
@@ -4,36 +4,95 @@ import { transporter, mailOptions } from '@/nodemailer'
 // Handles POST requests to /api
 
 const htmlContent = (data: any) => `
-  <html>
-    <head>
-      <style>
+<html>
+
+<head>
+    <style>
         body {
-          font-family: Arial, sans-serif;
+            font-family: Arial, sans-serif;
         }
+
         .header {
-          background-color: #3498db;
-          color: white;
-          padding: 10px;
-          text-align: center;
+            padding: 10px;
+            text-align: center;
         }
+        .header img{
+            width: 70%;
+            margin-left: 15%;
+            margin-right: 15%;
+        }
+
         .content {
-          padding: 20px;
+            padding: 20px;
+            margin-left: 15%;
+            margin-right: 15%;
         }
-        .saludo{
-          font-weight: 600;
+
+        .saludo {
+            font-weight: 600;
         }
-      </style>
-    </head>
-    <body>
-      <div class="header">
-        <h1>Cotizacion</h1>
-      </div>
-      <div>Hola ${data.name} <p>Informacion: ${data.answer}</p> ${
-  data.price ? ` <p>precio: ${data.price}<p>` : ''
-} 
-      </div>
-    </body>
-  </html>
+
+        .informacion {
+            background-color: #cfcfcf;
+            border-radius: 15px;
+            margin: 15px 0;
+            padding: 15px;
+        }
+        .rta{
+            font-style: italic;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="header">
+        <img src="https://firebasestorage.googleapis.com/v0/b/mas-eu-6aa58.appspot.com/o/sitio%2FEncabezado.png?alt=media&token=036121e9-4fc8-44e6-bc0a-033f216ab20a"
+            alt="Encabezado" />
+    </div>
+    <div class="content">
+        <p class="saludo">Estimado cliente ${data.name},</p>
+        <div>
+            <p>
+                Es un placer informarte que hemos creado una cotización personalizada para el servicio que solicitaste
+                en nuestra página web. El precio total para el proyecto es de <strong>${data.price}</strong>, que incluye todos los
+                detalles y beneficios que hemos diseñado especialmente para ti.
+            </p>
+            <p>Nuestro especialista ha dejado las siguientes observaciones: <p class="rta">${data.answer}</p></p>
+            <p>
+                En MAS E.U, nos enorgullece ofrecer soluciones confiables y de alta calidad. Nuestro
+                equipo de expertos se ha esforzado por crear una propuesta que se ajuste a tus necesidades y objetivos.
+                Aquí algunos motivos por los que deberías considerar nuestra oferta:
+
+                <p><strong>Calidad Garantizada:</strong> Nuestro servicio está respaldado por años de experiencia y un historial probado de
+                satisfacción del cliente. Puedes confiar en que cumpliremos con tus expectativas y más.</p>
+                <p><strong>Atención Personalizada:</strong> No somos solo una empresa, somos tus aliados. Estamos aquí para escucharte,
+                responder a tus preguntas y adaptarnos a tus requerimientos específicos.</p>
+                <p><strong>Transparencia:</strong> Hemos detallado cada componente de la cotización para que sepas exactamente en qué estás
+                invirtiendo. Sin sorpresas ni costos ocultos.</p>
+                <p><strong>Tiempo de Entrega:</strong>Trabajamos eficientemente para cumplir con los plazos acordados. Tu satisfacción es
+                nuestra prioridad.</p> 
+                <p>Te invitamos a considerar nuestra propuesta y a tomar una decisión informada. Si tienes alguna pregunta
+                o necesitas más información, no dudes en contactarnos. Estamos ansiosos por comenzar este proyecto
+                contigo.</p>
+                <br/>
+                <p>¡Gracias por confiar en nosotros!</p>
+                <br/> 
+                <p>Atentamente, El equipo de MAS E.U.</p>
+            </p>
+            
+        </div>
+
+        <div class="informacion">
+            <strong>Mantenimientos Asesorías y Servicios E.U.</strong>
+            <p><strong>Telefax:</strong> 6412880</p>
+            <p><strong>Teléfono:</strong> 6444372</p>
+            <p><strong>Celular:</strong> 3102554944</p>
+        </div>
+
+    </div>
+</body>
+
+</html>
 `
 
 export async function POST(request: Request) {
